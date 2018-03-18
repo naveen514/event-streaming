d41304500d7959c7567ab16f3d733fb624a4adad
rakesh1988 <rakesh.pelluri22@gmail.com>
2018-03-18 17:23:38 +0530

    added consumer

---
+++ kafka-stream-consumer/kfsconsumer.js
@@ -0,0 +1,20 @@
+var kafka = require('kafka-node');
+//var Consumer = kafka.Consumer;
+var client = new kafka.Client("192.168.1.7:2181");
+//console.log(client)
+var consumer = new kafka.Consumer(
+        client,
+        [
+            { topic: 'twitter_stream_test1', partition: 0 }
+        ],
+        {
+            autoCommit: false
+        }
+    );
+consumer.on('message', function (message) {
+  console.log(message);
+});
+
+consumer.on('error', function (err) {
+  console.log('error', err);
+});
